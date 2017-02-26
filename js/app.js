6f11e28f06708b3045563edcb56eaf53ae8a990f
Dylan K Tran <dtran2386@gmail.com>
2017-02-26 18:59:25 -0500

    deleted trash can icon and added bootstrap tooltip instead

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', []);
 
 app.controller('ctrl', function($scope) {
-  $scope.rows = ['Paul','John','Lucie'];
+  $scope.rows = ['Wake up','Get Ready','Go to work'];
   $scope.temp = false;
   
   $scope.addRow = function(){
@@ -28,4 +28,19 @@ app.controller('ctrl', function($scope) {
   $scope.isTemp = function(i){
     return i==$scope.rows.length-1 && $scope.temp;
   };
+});
+
+app.directive('tooltip', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            $(element).hover(function(){
+                // on mouseenter
+                $(element).tooltip('show');
+            }, function(){
+                // on mouseleave
+                $(element).tooltip('hide');
+            });
+        }
+    };
 });
